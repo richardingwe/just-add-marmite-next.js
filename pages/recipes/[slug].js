6e83199eba22913c32307f5d5a885905119979cb
richardingwe <richardingwe5@gmail.com>
2021-04-16 03:43:46 +0100

    added styles to slug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -50,7 +50,7 @@ export default function RecipeDetails({ recipe }) {
 				<Image
 					src={`https:${featuredImage.fields.file.url}`}
 					width='1200px'
-					height='800px'
+					height='400px'
 				/>
 				<h2>{title}</h2>
 			</div>
@@ -66,6 +66,33 @@ export default function RecipeDetails({ recipe }) {
 				<h3>Method:</h3>
 				<div>{documentToReactComponents(method)}</div>
 			</div>
+
+			<style jsx>{`
+				h2,
+				h3 {
+					text-transform: uppercase;
+				}
+				.banner h2 {
+					margin: 0;
+					background: #fff;
+					display: inline-block;
+					padding: 20px;
+					position: relative;
+					top: -60px;
+					left: -10px;
+					transform: rotateZ(-1deg);
+					box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+				}
+				.info p {
+					margin: 0;
+				}
+				.info span::after {
+					content: ', ';
+				}
+				.info span:last-child::after {
+					content: '.';
+				}
+			`}</style>
 		</div>
 	);
 }

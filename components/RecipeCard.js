05fbba22dfb642c92df069a391b0c4dcd93515a4
richardingwe <richardingwe5@gmail.com>
2021-04-16 02:51:28 +0100

    added styles for recipeCards

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -18,7 +18,7 @@ const RecipeCard = ({ recipe }) => {
 			<div className='content'>
 				<div className='info'>
 					<h4>{title}</h4>
-					<p>Takes approx {cookingTime} mins to make</p>
+					<p>Takes approx {cookingTime}mins to make</p>
 				</div>
 				<div className='actions'>
 					<Link href={`/recipes/${slug}`}>
@@ -26,6 +26,42 @@ const RecipeCard = ({ recipe }) => {
 					</Link>
 				</div>
 			</div>
+
+			<style jsx>{`
+				.card {
+					transform: rotateZ(-1deg);
+				}
+				.content {
+					background: #fff;
+					box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+					margin: 0;
+					position: relative;
+					top: -40px;
+					left: -10px;
+				}
+				.info {
+					padding: 16px;
+				}
+				.info h4 {
+					margin: 4px 0;
+					text-transform: uppercase;
+				}
+				.info p {
+					margin: 0;
+					color: #777;
+				}
+				.actions {
+					margin-top: 20px;
+					display: flex;
+					justify-content: flex-end;
+				}
+				.actions a {
+					color: #fff;
+					background: #f01b29;
+					padding: 16px 24px;
+					text-decoration: none;
+				}
+			`}</style>
 		</div>
 	);
 };

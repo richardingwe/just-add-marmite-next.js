78787aed3053ddbbbb74c61b13d2b4726362b10b
richardingwe <richardingwe5@gmail.com>
2021-04-16 03:52:33 +0100

    corrected the nex/image import

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,5 +1,5 @@
 import { createClient } from 'contentful';
-import Image from 'next/Image';
+import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({

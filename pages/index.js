ac893df4fd71d595e689732bba7abc63a2677aed
richardingwe <richardingwe5@gmail.com>
2021-04-16 01:44:26 +0100

    connected to contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+	const client = createClient({
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+	});
+}
+
 export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+	return <div className='recipe-list'>Recipe List</div>;
 }

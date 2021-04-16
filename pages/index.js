fc35c89576b589e7dd73aeee12fad2cd6588226b
richardingwe <richardingwe5@gmail.com>
2021-04-16 01:59:48 +0100

    get recipes

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,19 @@ export async function getStaticProps() {
 		space: process.env.CONTENTFUL_SPACE_ID,
 		accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 	});
+
+	const res = await client.getEntries({
+		content_type: 'receipe',
+	});
+
+	return {
+		props: {
+			recipes: res.items,
+		},
+	};
 }
 
-export default function Recipes() {
+export default function Recipes({ recipes }) {
+	console.log(recipes);
 	return <div className='recipe-list'>Recipe List</div>;
 }

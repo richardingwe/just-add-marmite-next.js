d8a165f000940829213a0c7fc597f805011c7a3e
richardingwe <richardingwe5@gmail.com>
2021-04-16 02:38:05 +0100

    added next.js config and got other recipes details

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/Image';
 
 const RecipeCard = ({ recipe }) => {
 	const { title, slug, cookingTime, thumbnail } = recipe.fields;
 
 	return (
 		<div className='card'>
-			<div className='featured'>{/* {image - thumb} */}</div>
+			<div className='featured'>
+				<Image
+					src={`https:${thumbnail.fields.file.url}`}
+					width={thumbnail.fields.file.details.image.width}
+					height={thumbnail.fields.file.details.image.height}
+				/>
+			</div>
 			<div className='content'>
 				<div className='info'>
 					<h4>{title}</h4>
